954146db9204f901c0fda9fb540a0c9310283aba
wasborn200 <archibald.ff1@gmail.com>
2021-07-07 10:32:23 +0900

    page288 end

---
+++ out/_next/static/kRJsmbsdO4XoI_KPG3Zd0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7b9e49a03f9dfa290ace.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+export default () =>
+  <Layout header="Next" title="Top page.">
+    <p>Welcome to next.js!</p>
+    <hr/>
+    <Link href="./other"><button>
+      go to Other &gt;&gt;
+    </button></Link>
+  </Layout>
